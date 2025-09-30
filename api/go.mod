7340fe09bad08c221473a601198d2f81c59676b8
ranjun <ranjun@boohee.com>
2025-09-30 15:45:26 +0800

    mr

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/openexw/dify-go/api
+
+go 1.24.7
+
+require google.golang.org/protobuf v1.36.9
--- chatflow/chatflow.go
+++
@@ -1,9 +0,0 @@
-package chatflow
-
-type Chatflow interface {
-}
-type chatflow struct{}
-
-func NewChatflow() Chatflow {
-	return &chatflow{}
-}
